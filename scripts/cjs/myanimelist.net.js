1744d5e3aaa8fdce9cbc42f3376d833c6f0af449
Ilya <dogzy123@mail.ru>
2016-07-23 12:06:27 +0300

    Removed files moved to separate repository called fix-web-sites

--- scripts/cjs/myanimelist.net.js
+++
@@ -1,9 +0,0 @@
-/* removing ads placeholders, they take too much space, reviews don't fit into screen */
-
-var $$ = s => document.querySelectorAll(s);
-
-setInterval(() => {
-    for (var el of Array.from($$('._unit'))) {
-        el.style.display = 'none';
-    }
-}, 500);
--- scripts/cjs/nnm-club.me.js
+++
@@ -1,9 +0,0 @@
-var $$ = s => Array.from(document.querySelectorAll(s));
-
-// TODO: remove interval when ads appeared and removed
-// TODO: css instead of interval to make them not appear at all
-
-setInterval(() => {
-    $$('body > *').forEach(el => el.tagName === 'NOINDEX' ? el.innerHTML = '' : 0);
-    $$('center').forEach(el => el.style.display = 'none');
-}, 500);
