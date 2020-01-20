084a00812dc5c4572701c214ce4b4f5b68b1ae0f
Artur Klesun <arturklesun@gmail.com>
2020-01-21 01:51:41 +0200

    Few improvements in HTTP transportcode

--- server.js
+++ server.js
@@ -18,85 +18,138 @@ const readPost = (rq) => new Promise((ok, err) => {
 	}
 });
 
+const isStaticFile = pathname => {
+	return pathname.startsWith('/entry/')
+		|| pathname.startsWith('/src/')
+		|| pathname.startsWith('/libs/')
+		|| pathname.startsWith('/out/')
+		|| pathname.startsWith('/imgs/')
+		|| pathname.startsWith('/unv/hosted/')
+		|| pathname.startsWith('/unv/imagesFromWeb/')
+		|| pathname.startsWith('/tests/')
+		|| pathname.startsWith('/Dropbox/web/')
+		|| pathname.startsWith('/node_modules/')
+		|| pathname === '/favicon.ico'
+		|| pathname === '/robots.txt';
+};
+
+const redirect = (rs, url) => {
+	rs.writeHead(302, {
+		'Location': url,
+	});
+	rs.end();
+};
+
+const serveStaticFile = async (pathname, rs) => {
+	pathname = decodeURIComponent(pathname);
+	let absPath = __dirname + pathname;
+	if (absPath.endsWith('/')) {
+		absPath += 'index.html';
+	}
+	if (!fsSync.existsSync(absPath)) {
+		return Rej.NotFound('File ' + pathname + ' does not exist');
+	}
+	if ((await fs.lstat(absPath)).isDirectory()) {
+		return redirect(rs, pathname + '/');
+	}
+	const bytes = await fs.readFile(absPath);
+	if (absPath.endsWith('.html')) {
+		rs.setHeader('Content-Type', 'text/html');
+	} else if (absPath.endsWith('.css')) {
+		rs.setHeader('Content-Type', 'text/css');
+	} else if (absPath.endsWith('.js')) {
+		rs.setHeader('Content-Type', 'text/javascript');
+	} else if (absPath.endsWith('.svg')) {
+		rs.setHeader('Content-Type', 'image/svg+xml');
+	}
+	rs.end(bytes);
+};
+
+const servePythonScript = (rq, rs) => {
+	return new Promise(async (resolve, reject) => {
+		PythonShell.run(__dirname + '/htbin/json_service.py', {
+			args: [JSON.stringify({
+				override_environ: {
+					CONTENT_LENGTH: rq.headers['content-length'],
+					QUERY_STRING: url.parse(rq.url).query,
+				},
+				post_string: await readPost(rq),
+			})],
+		}, (err, results) => {
+			if (err) {
+				reject(err);
+			} else {
+				const responseWithHeaders = (results || []).join('\n');
+				const [headersPart, bodyPart] = responseWithHeaders.split('\n\n');
+				for (const headerLine of !headersPart ? [] : headersPart.split('\n')) {
+					const [key, value] = headerLine.split(': ');
+					rs.setHeader(key, value);
+				}
+				rs.end(bodyPart);
+				resolve();
+			}
+		});
+	});
+};
+
+const serveExploit = (rq, rs) => {
+	const clientIp = rq.connection.remoteAddress
+		|| rq.socket.remoteAddress
+		|| (rq.connection.socket || {}).remoteAddress;
+	const forwardIp = rq.headers['x-forwarded-for'];
+	const record = {
+		exploitPath: url.parse(rq.url).path,
+		method: rq.method, clientIp, forwardIp,
+	};
+	rq.destroy();
+	rs.end();
+	console.warn(JSON.stringify(record));
+};
+
+const serveCors = (rs) => {
+	rs.setHeader('Content-Type', 'application/json');
+	rs.setHeader('Access-Control-Allow-Origin', '*');
+	rs.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	rs.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	rs.setHeader('Access-Control-Allow-Credentials', true);
+};
+
+const apiRoutes = {
+	'/api/orderSoftware': (rq) => {
+		return Rej.NotImplemented('Sorry, form submission is not supported yet');
+	},
+};
+const redirects = {
+	'/': '/entry/',
+	'/entry/main/': '/entry/',
+};
+
 /**
  * @param {http.IncomingMessage} rq
  * @param {http.ServerResponse} rs
  */
 const handleRq = async (rq, rs) => {
 	const parsedUrl = url.parse(rq.url);
-	let pathname = parsedUrl.pathname;
+	const pathname = parsedUrl.pathname;
 
-	const redirect = url => {
-		rs.writeHead(302, {
-			'Location': url,
+	const redirectUrl = redirects[pathname];
+	const apiAction = apiRoutes[pathname];
+
+	if (rq.method === 'OPTIONS') {
+		return serveCors(rs);
+	} else if (redirectUrl) {
+		return redirect(rs, redirectUrl);
+	} else if (apiAction) {
+		return apiAction(rq).then(result => {
+			rs.statusCode = 200;
+			rs.end(JSON.stringify(result));
 		});
-		rs.end();
-	};
-
-	if (pathname === '/') {
-		return redirect('/entry/');
-	} else if (pathname.startsWith('/entry/')
-			|| pathname.startsWith('/src/')
-			|| pathname.startsWith('/libs/')
-			|| pathname.startsWith('/out/')
-			|| pathname.startsWith('/imgs/')
-			|| pathname.startsWith('/unv/hosted/')
-			|| pathname.startsWith('/unv/imagesFromWeb/')
-			|| pathname.startsWith('/tests/')
-			|| pathname.startsWith('/Dropbox/web/')
-			|| pathname.startsWith('/node_modules/')
-			|| pathname === '/favicon.ico'
-			|| pathname === '/robots.txt'
-	) {
-		pathname = decodeURIComponent(pathname);
-		let absPath = __dirname + pathname;
-		if (absPath.endsWith('/')) {
-			absPath += 'index.html';
-		}
-		if (!fsSync.existsSync(absPath)) {
-			return Rej.NotFound('File ' + pathname + ' does not exist');
-		}
-		if ((await fs.lstat(absPath)).isDirectory()) {
-			return redirect(pathname + '/');
-		}
-		const bytes = await fs.readFile(absPath);
-		if (absPath.endsWith('.html')) {
-			rs.setHeader('Content-Type', 'text/html');
-		} else if (absPath.endsWith('.css')) {
-			rs.setHeader('Content-Type', 'text/css');
-		} else if (absPath.endsWith('.js')) {
-			rs.setHeader('Content-Type', 'text/javascript');
-		} else if (absPath.endsWith('.svg')) {
-			rs.setHeader('Content-Type', 'image/svg+xml');
-		}
-		rs.end(bytes);
+	} else if (isStaticFile(pathname)) {
+		return serveStaticFile(pathname);
 	} else if (pathname === '/htbin/json_service.py') {
-		return new Promise(async (resolve, reject) => {
-			PythonShell.run(__dirname + '/htbin/json_service.py', {
-				args: [JSON.stringify({
-					override_environ: {
-						CONTENT_LENGTH: rq.headers['content-length'],
-						QUERY_STRING: parsedUrl.query,
-					},
-					post_string: await readPost(rq),
-				})],
-			}, (err, results) => {
-				if (err) {
-					reject(err);
-				} else {
-					const responseWithHeaders = (results || []).join('\n');
-					const [headersPart, bodyPart] = responseWithHeaders.split('\n\n');
-					for (const headerLine of !headersPart ? [] : headersPart.split('\n')) {
-						const [key, value] = headerLine.split(': ');
-						rs.setHeader(key, value);
-					}
-					rs.end(bodyPart);
-					resolve();
-				}
-			});
-		});
+		return servePythonScript(rq, rs);
 	} else {
-		throw new Error('No routes matched');
+		return serveExploit(rq, rs);
 	}
 };
 
