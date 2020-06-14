0f432f666fc86e2fc1ab00df148ea096379652fc
Artur Klesun <arturklesun@gmail.com>
2020-06-14 15:56:33 +0300

    And one more

--- entry/trilemma/index.js
+++ entry/trilemma/index.js
@@ -2,6 +2,14 @@
 import {Svg} from './src/Dom.js';
 import MapGenerator from "./src/MapGenerator.js";
 
+const gui = {
+    tileMapHolder: document.querySelector('.tile-map-holder'),
+    turnsLeftHolder: document.querySelector('.turns-left-holder'),
+    playerList: document.querySelector('.player-list'),
+};
+
+// move to TileMapDisplay.js: start
+
 const resourceSvgs = {
     WHEAT: (isEven) => Svg('path',{
         d: 'm512 33.83v-18.83c0-8.284-6.716-15-15-15h-18.831c-46.329 0-83.766 24.687-104.726 56.938-5.488-12.033-13.141-23.111-22.772-32.738-10.454-10.461-19.798-19.806-19.798-19.806-5.857-5.858-15.355-5.858-21.213 0l-19.8 19.8c-21.147 21.138-32.793 49.255-32.793 79.173 0 7.704.778 15.287 2.286 22.664-1.629-1.856-3.319-3.67-5.082-5.432l-19.797-19.805c-5.858-5.859-15.361-5.853-21.214 0l-19.8 19.8c-21.147 21.138-32.793 49.256-32.793 79.173 0 7.704.778 15.288 2.286 22.665-1.629-1.856-3.319-3.67-5.082-5.432l-19.797-19.806c-2.813-2.813-6.628-4.394-10.606-4.394-3.978 0-7.793 1.581-10.606 4.394l-19.801 19.8c-43.278 43.259-44.195 114.169-.002 158.344l9.196 9.196-101.861 101.86c-5.858 5.858-5.858 15.355 0 21.213 5.857 5.858 15.355 5.858 21.213 0l101.858-101.858 9.193 9.196c21.153 21.144 49.27 32.789 79.168 32.789 12.39 0 48.28-1.905 79.179-32.791l19.803-19.803c2.813-2.813 4.394-6.628 4.394-10.606 0-3.979-1.581-7.794-4.394-10.607l-19.805-19.804c-1.76-1.76-3.573-3.447-5.427-5.074 7.373 1.507 14.952 2.285 22.65 2.285h.008c29.899 0 58.015-11.645 79.171-32.792l19.802-19.802c5.858-5.858 5.853-15.361 0-21.214l-19.804-19.804c-1.76-1.76-3.574-3.447-5.427-5.074 7.373 1.507 14.952 2.285 22.65 2.285h.008c29.899 0 58.016-11.645 79.171-32.791l19.802-19.802c5.858-5.857 5.858-15.355 0-21.213l-19.805-19.805c-9.635-9.631-20.717-17.283-32.752-22.769 32.225-20.945 56.95-58.324 56.95-104.723zm-200.929 11.579 9.194-9.194c2.791 2.792 5.937 5.94 9.191 9.197 32.011 31.997 32.071 83.854-.006 115.917l-9.186 9.189-9.195-9.196c-31.73-31.715-32.31-83.614.002-115.913zm-26.289 131.704c1.628 1.853 3.316 3.666 5.076 5.425l39.598 39.606c1.761 1.76 3.574 3.448 5.428 5.075-22.801-4.661-41.561-1.187-51.576 1.473 2.667-10.043 6.13-28.8 1.474-51.579zm-70.111-35.304 9.193-9.194 9.192 9.196c32.011 31.997 32.071 83.854-.006 115.917l-9.186 9.189-9.195-9.196c-31.723-31.709-32.317-83.606.002-115.912zm-26.29 131.702c1.628 1.854 3.316 3.666 5.077 5.426l39.598 39.607c1.761 1.76 3.574 3.448 5.428 5.075-22.801-4.661-41.561-1.187-51.578 1.473 2.668-10.043 6.132-28.8 1.475-51.581zm-51.731 80.618-9.186 9.189-9.196-9.196c-31.72-31.707-32.32-83.604.002-115.913l9.194-9.194 9.192 9.196c32.011 31.996 32.071 83.854-.006 115.918zm137.143 39.598c-15.487 15.481-36.071 24.006-57.959 24.006-9.159 0-35.385-1.434-57.962-24l-9.196-9.2 9.19-9.193c32.021-32.008 83.856-32.059 115.926-.002l9.196 9.196zm96.4-96.4c-31.791 31.777-83.647 32.267-115.922.006l-9.195-9.2 9.189-9.193c32.022-32.009 83.855-32.059 115.926-.002l9.196 9.196zm96.398-114.789 9.195 9.195-9.193 9.193c-31.793 31.78-83.652 32.263-115.922.006-3.255-3.258-6.404-6.408-9.195-9.201l9.189-9.192c32.023-32.009 83.857-32.056 115.926-.001zm-79.302-54.005h-3.822v-3.83c0-52.401 42.418-94.703 94.703-94.703h3.83v3.83c0 52.702-42.805 94.703-94.711 94.703z',
@@ -35,6 +43,72 @@ const resourceSvgs = {
     }),
 };
 
+const makeTile = (x, y, isEven) => {
+    const makePoly = (attrs) => {
+        let relX = TILE_WIDTH / 2;
+        let relY = 0;
+
+        if (!isEven) {
+            relY += TILE_HEIGHT / 3;
+        } else {
+            relY += TILE_HEIGHT * 2 / 3;
+        }
+        return Svg('polygon', {
+            'transform-origin': relX + 'px ' + relY + '0px',
+            points: [
+                {dx: -TILE_WIDTH / 2, dy: TILE_HEIGHT / 3},
+                {dx: +TILE_WIDTH / 2, dy: TILE_HEIGHT / 3},
+                {dx: 0, dy: -TILE_HEIGHT * 2 / 3},
+            ].map(
+                ({dx, dy}) => [
+                    relX + dx,
+                    relY + dy * (isEven ? 1 : -1),
+                ].map(n => n.toFixed(3)).join(',')
+            ).join(' '),
+            ...attrs,
+        });
+    };
+    return Svg('svg', {x, y}, [
+        makePoly({class: 'base-tile'}),
+        makePoly({class: 'effects-overlay'}),
+    ]);
+};
+
+const initMatrix = (boardConfig) => {
+    const matrix = [];
+
+    const ROWS = boardConfig.totalRows;
+    const BOARD_WIDTH_PX = ROWS * TILE_WIDTH;
+    const BOARD_HEIGHT_PX = ROWS * TILE_HEIGHT;
+
+    gui.tileMapHolder.style.width = BOARD_WIDTH_PX + 'px';
+    gui.tileMapHolder.style.height = BOARD_HEIGHT_PX + 'px';
+
+    for (const {row, col, resource} of boardConfig.tiles) {
+        const x = (col  - row - 1) * TILE_WIDTH / 2;
+        const y = row * TILE_HEIGHT;
+        const isEven = col % 2 === 0;
+        const svgEl = makeTile(BOARD_WIDTH_PX / 2 + x, y, isEven);
+        const svgResource = resourceSvgs[resource];
+
+        // assign svg icon to resource tile
+        if (svgResource) {
+            svgEl.appendChild(svgResource(isEven));
+        }
+
+        svgEl.setAttribute('data-resource', resource);
+
+        gui.tileMapHolder.appendChild(svgEl);
+
+        matrix[row] = matrix[row] || {};
+        matrix[row][col] = {row, col, svgEl};
+    }
+
+    return matrix;
+};
+
+// move to TileMapDisplay.js: end
+
 const getInput = () => new Promise((ok,err) => {
     const listener = (evt) => {
         let removeListener = true;
@@ -67,12 +141,6 @@ const TILE_HEIGHT = Math.sqrt(3) * TILE_WIDTH / 2;
 const RESOURCES = ['WHEAT', 'OIL', 'GOLD'];
 const PLAYER_CODE_NAMES = ['DARK', 'GREY', 'LIGHT'];
 const HOT_SEAT = true;
-
-const gui = {
-    tileMapHolder: document.querySelector('.tile-map-holder'),
-    turnsLeftHolder: document.querySelector('.turns-left-holder'),
-    playerList: document.querySelector('.player-list'),
-};
 
 const getBoardConfiguration = async () => {
     if (HOT_SEAT) {
@@ -128,75 +196,10 @@ const updateStatsTable = (pendingPlayer, playerResources) => {
 };
 
 (async () => {
-
     const boardConfig = await getBoardConfiguration();
 
-    const ROWS = boardConfig.totalRows;
-    const BOARD_WIDTH_PX = ROWS * TILE_WIDTH;
-    const BOARD_HEIGHT_PX = ROWS * TILE_HEIGHT;
-
-    const makeTile = (x, y, isEven) => {
-        const makePoly = (attrs) => {
-            let relX = TILE_WIDTH / 2;
-            let relY = 0;
-
-            if (!isEven) {
-                relY += TILE_HEIGHT / 3;
-            } else {
-                relY += TILE_HEIGHT * 2 / 3;
-            }
-            return Svg('polygon', {
-                'transform-origin': relX + 'px ' + relY + '0px',
-                points: [
-                    {dx: -TILE_WIDTH / 2, dy: TILE_HEIGHT / 3},
-                    {dx: +TILE_WIDTH / 2, dy: TILE_HEIGHT / 3},
-                    {dx: 0, dy: -TILE_HEIGHT * 2 / 3},
-                ].map(
-                    ({dx, dy}) => [
-                        relX + dx,
-                        relY + dy * (isEven ? 1 : -1),
-                    ].map(n => n.toFixed(3)).join(',')
-                ).join(' '),
-                ...attrs,
-            });
-        };
-        return Svg('svg', {x, y}, [
-            makePoly({class: 'base-tile'}),
-            makePoly({class: 'effects-overlay'}),
-        ]);
-    };
-
-    const initMatrix = () => {
-        const matrix = [];
-
-        gui.tileMapHolder.style.width = BOARD_WIDTH_PX + 'px';
-        gui.tileMapHolder.style.height = BOARD_HEIGHT_PX + 'px';
-
-        for (const {row, col, resource} of boardConfig.tiles) {
-            const x = (col  - row - 1) * TILE_WIDTH / 2;
-            const y = row * TILE_HEIGHT;
-            const isEven = col % 2 === 0;
-            const svgEl = makeTile(BOARD_WIDTH_PX / 2 + x, y, isEven);
-            const svgResource = resourceSvgs[resource];
-
-            // assign svg icon to resource tile
-            if (svgResource) {
-                svgEl.appendChild(svgResource(isEven));
-            }
-
-            svgEl.setAttribute('data-resource', resource);
-
-            gui.tileMapHolder.appendChild(svgEl);
-
-            matrix[row] = matrix[row] || {};
-            matrix[row][col] = {row, col, svgEl};
-        }
-
-        return matrix;
-    };
-
     const main = async () => {
-        const matrix = initMatrix();
+        const matrix = initMatrix(boardConfig);
 
         const getTile = ({x, y}) => {
             return (matrix[y] || {})[x] || null;
